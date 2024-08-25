900ec2cbe906cd3edfea7bc7db0f356334c4392b
dev-isaac <dev.isaacwoo@gmail.com>
2024-08-25 15:19:31 +0900

    tests: add BBox init tests

--- tests/test_bbox.py
+++ tests/test_bbox.py
@@ -1,1 +1,93 @@
 import pytest
+from pydantic_core import ValidationError
+
+from bboxes import BBox, OriginType
+
+
+@pytest.mark.parametrize(
+    argnames=["origin_type"],
+    argvalues=[
+        pytest.param(OriginType.TOP_LEFT),
+        pytest.param(OriginType.BOTTOM_RIGHT),
+    ],
+)
+class TestBBoxInit:
+
+    @pytest.mark.parametrize(
+        argnames=["xmin", "ymin", "xmax", "ymax"],
+        argvalues=[
+            pytest.param(
+                -1,
+                0,
+                1,
+                3,
+                id="negative min",
+            ),
+            pytest.param(
+                5,
+                0,
+                1,
+                2,
+                id="xmin greater than xmax"
+            ),
+            pytest.param(
+                0,
+                0,
+                0,
+                0,
+                id="origin, zero-area point",
+            ),
+        ],
+    )
+    def test_invalid_bbox_init(
+        self, origin_type: OriginType, xmin: float, ymin: float, xmax: float, ymax: float
+    ):
+        with pytest.raises(expected_exception=ValidationError):
+            BBox(
+                origintype=origin_type,
+                xmin=xmin,
+                ymin=ymin,
+                xmax=xmax,
+                ymax=ymax,
+            )
+
+
+    @pytest.mark.parametrize(
+        argnames=["xmin", "ymin", "xmax", "ymax"],
+        argvalues=[
+            pytest.param(
+                0,
+                0,
+                1,
+                2,
+            ),
+            pytest.param(
+                0.0,
+                0.0,
+                1.5,
+                2.5,
+            ),
+            pytest.param(
+                10.0,
+                10.01,
+                10.5,
+                12.5,
+            ),
+            pytest.param(
+                10,
+                10,
+                10.01,
+                12.5,
+            ),
+        ],
+    )
+    def test_bbox_init(
+        self, origin_type: OriginType, xmin: float, ymin: float, xmax: float, ymax: float
+    ):
+        BBox(
+            origintype=origin_type,
+            xmin=xmin,
+            ymin=ymin,
+            xmax=xmax,
+            ymax=ymax,
+        ),
